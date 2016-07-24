f70d3ca780447975bb9206f9248c781295e9965d
Brian Donovan <me@brian-donovan.com>
2016-07-24 11:38:59 -0700

    chore: update website on build

---
+++ script/helpers.sh
@@ -0,0 +1,18 @@
+PATH="$(npm bin):$PATH"
+
+hasChanges() {
+  git status >/dev/null # update the cache
+  if git diff-index --quiet HEAD --; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+configureGithubRemote() {
+  if [ -n "$GH_TOKEN" ]; then
+    git remote set-url origin "https://$GH_TOKEN@github.com/$1.git"
+    git config user.name "Brian Donovan"
+    git config user.email "me@brian-donovan.com"
+  fi
+}
